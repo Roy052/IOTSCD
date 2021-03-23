833891764c91747e5d7d7bb2612cdf90c77ceb1c
Roy052 <32808714+Roy052@users.noreply.github.com>
2021-03-23 22:13:01 +0900

    FirstMove..

---
+++ Assets/Great_Escape/Script/Playermove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermove: MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        transform.Translate(new Vector2(inputX, inputY) * Time.deltaTime * moveSpeed);
+    }
+}
