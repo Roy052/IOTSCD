c7ad767347ae6bbec31fc29fc38a4f7ca394cdf7
crihit <rlagusgkr98@gmail.com>
2021-03-28 20:57:27 +0900

    Implement card flipping

---
+++ Assets/CardFilp/Scripts/CardFlipper.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFlipper : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    CardModel model;
+    public AnimationCurve scaleCurve;
+    public float duration = 0.5f;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        model = GetComponent<CardModel>();
+    }
+
+    public void FlipCard(Sprite startImage, Sprite EndImage, int cardIndex)
+    {
+        StopCoroutine(Flip(startImage, EndImage, cardIndex));
+        StartCoroutine(Flip(startImage, EndImage, cardIndex));
+    }
+
+    IEnumerator Flip(Sprite startImage, Sprite endImage, int cardIndex)
+    {
+        spriteRenderer.sprite = startImage;
+        float time = 0f;
+        while(time <= 1)
+        {
+            float scale = scaleCurve.Evaluate(time);
+            time = time + Time.deltaTime / duration;
+            Vector3 localScale = transform.localScale;
+            localScale.x = scale;
+            transform.localScale = localScale;
+            
+            if(time>=0.5f)
+            {
+                spriteRenderer.sprite = endImage;
+            }
+            yield return new WaitForFixedUpdate();
+        }
+        if(cardIndex==-1)
+        {
+            model.cardIndex = cardIndex;
+            model.ToggleFace(true);
+        }
+    }
+}
---
+++ Assets/CardFilp/Scripts/DebugChangeCard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugChangeCard : MonoBehaviour
+{
+    CardFlipper flipper;
+    CardModel cardModel;
+    int cardIndex = 0;
+    public GameObject card;
+
+    void Awake()
+    {
+        cardModel = card.GetComponent<CardModel> ();
+        flipper = card.GetComponent<CardFlipper> ();
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button (new Rect (10, 10, 100, 28),"클릭하세요"))
+        {
+            if(cardIndex >= cardModel.faces.Length)
+            {
+                cardIndex = 0;
+                flipper.FlipCard(cardModel.faces[cardModel.faces.Length-1],cardModel.cardBack, -1);
+            }
+            else
+            {
+                if(cardIndex > 0)
+                {
+                    flipper.FlipCard(cardModel.faces[cardIndex-1],cardModel.faces[cardIndex],cardIndex);
+                }
+                else
+                {
+                    flipper.FlipCard(cardModel.cardBack,cardModel.faces[cardIndex],cardIndex);
+                }
+                cardIndex++;
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
