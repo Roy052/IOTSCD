ac85c2801de0f13b1e2a44e41a62c6a37b6e246c
Roy052 <32808714+Roy052@users.noreply.github.com>
2021-04-06 12:32:26 +0900

    Wall_Finish_Button

---
+++ Assets/Great_Escape/Script/DirectionButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DirectionButton : MonoBehaviour
+{
+    Button button;
+    // Start is called before the first frame update
+    public void onClickButton()
+    { 
+        Debug.Log(this.gameObject.name);
+    }
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(onClickButton);
+    }
+}
