4596ec3bbc8a8d92f45077e7f0aba4ff18329a86
Roy052 <32808714+Roy052@users.noreply.github.com>
2021-04-26 13:58:39 +0900

    UsingRaycast_MoveSmoothly

--- Assets/Great_Escape/Script/Playermove.cs
+++ Assets/Great_Escape/Script/Playermove.cs
@@ -17,6 +17,9 @@ public class Playermove: MonoBehaviour
     int[,] clockwiseMove  = new int[4,2] { {-1, 0},{0 , 1}, { 1, 0 }, { 0, -1 } };
     int direction = 0;
 
+    //Raycasting
+    [SerializeField] private LayerMask layermask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,30 +37,37 @@ public class Playermove: MonoBehaviour
 
     public IEnumerator Movement()
     {
+        //게임이 부드러워 보이기 위해 약간의 지연시간
+        float bufferedTime = (float) 0.3;
         for (int i = 0; i < ordercount; i++)
         {
             Debug.Log(order[i]);
             if (order[i] == 0)
             {
-                
-                playerRigidBody.AddForce(new Vector2(clockwiseMove[direction, 0], clockwiseMove[direction, 1]) * moveSpeed * 100);
-                yield return new WaitForSeconds(3);
+                Vector2 movingDirection = new Vector2(clockwiseMove[direction, 0], clockwiseMove[direction, 1]);
+                RaycastHit2D hit;
+                hit = Physics2D.Raycast(this.transform.position, movingDirection, layermask);
+
+                playerRigidBody.AddForce(movingDirection * moveSpeed * 100);
+                Debug.Log(hit.distance + ", " + hit.transform.name);
+                float waitTime = hit.distance / (500 * Time.fixedDeltaTime);
+                yield return new WaitForSeconds(waitTime);
                 playerRigidBody.velocity = Vector2.zero;
             }
             if (order[i] == 1)
             {
                 if (direction == 0) direction = 3;
                 else direction = direction - 1;
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(bufferedTime);
             }
             if (order[i] == 2)
             {
                 direction = (direction + 1) % 4;
-                yield return new WaitForSeconds(1);
+                yield return new WaitForSeconds(bufferedTime);
             }
 
         }
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(bufferedTime);
         gamesystem.gameRestart();
 
     }
