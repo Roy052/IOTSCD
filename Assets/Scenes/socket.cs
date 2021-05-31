e8c86ee3f19ae5af2e9c5abd1f305691ea67f0e1
crihit <rlagusgkr98@gmail.com>
2021-05-31 10:33:03 +0900

    socket test update

--- Assets/Scenes/socket.cs
+++ Assets/Scenes/socket.cs
@@ -9,24 +9,44 @@ public class socket : MonoBehaviour
 {
     // Start is called before the first frame update
     UdpClient srv;
-    IPEndPoint remoteEP;        
+    IPEndPoint RemoteIpEndPoint;  // byte[] StrByte;   
+    public string arduinoIP = "192.168.0.11";
+    public int port = 8888;
+    private IEnumerator m_Coroutine;
     void Start()
     {
-        srv = new UdpClient(8888);
-        IPAddress ip1 = IPAddress.Parse("192.168.0.11");
-        remoteEP= new IPEndPoint(ip1, 0);
+        RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+        Debug.Log("start");
+        srv = new UdpClient(arduinoIP,port);
+        // IPEndPoint remoteEP= new IPEndPoint(ip1, 0);
 
         byte[] StrByte = Encoding.UTF8.GetBytes("hi");
-        srv.Send(StrByte,StrByte.Length,remoteEP);
+        int dat = srv.Send(StrByte,StrByte.Length);
+        Debug.Log("send data : "+dat.ToString()+"Bytes");
+
+        byte[] receiveBytes = srv.Receive(ref RemoteIpEndPoint);
+        string returnData = Encoding.ASCII.GetString(receiveBytes);
+        Debug.Log(returnData);
+        m_Coroutine = receiveUDP();
     }
 
     // Update is called once per frame
     void Update()
     {
-        byte[] dgram = srv.Receive(ref remoteEP);
-        Debug.Log("[Receive] "+remoteEP.ToString()+" 로부터 "+dgram.Length+" 바이트 수신");
+        StartCoroutine(m_Coroutine);
+    }
 
-        srv.Send(dgram, dgram.Length, remoteEP);
-        Debug.Log("[Send] "+remoteEP.ToString()+" 로부터 "+dgram.Length+" 바이트 수신");
+    IEnumerator receiveUDP(){
+        try{
+            Debug.Log("coroutine in");
+            byte[] receiveBytes = srv.Receive(ref RemoteIpEndPoint);
+            string returnData = Encoding.ASCII.GetString(receiveBytes);
+            Debug.Log(returnData);
+        }
+        catch{
+            Debug.Log("failed");
+        }
+        yield return null;
     }
 }
