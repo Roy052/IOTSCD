26518216869589bfabc78bc017483abffb334bc3
CheeseCake116 <33691228+CheeseCake116@users.noreply.github.com>
2021-06-02 00:55:51 +0900

    06/02 MouseCursorObj 추가
    커서는 숨기고 마우스 스프라이트가 마우스를 따라다니도록 함

---
+++ Assets/CatchMoles/Image/MouseCursorManage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursorManage : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseCursorObjScript.instance.setPosition(Input.mousePosition);
+    }
+}
---
+++ Assets/CatchMoles/Image/MouseCursorObjScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursorObjScript : MonoBehaviour
+{
+    public static MouseCursorObjScript instance;
+    public Sprite Hammer;
+    public Sprite Hammer_rotate;
+    private SpriteRenderer spriteRenderer;
+    Vector2 pos = new Vector2(0, 0);
+    Vector2 MousePos = new Vector2(0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Hammer_rotate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = false;
+        if (Input.GetMouseButton(0))
+        {
+            spriteRenderer.sprite = Hammer;
+            pos = Camera.main.ScreenToWorldPoint(new Vector3(MousePos.x + 30, MousePos.y - 30));
+        }
+        else
+        {
+            spriteRenderer.sprite = Hammer_rotate;
+            pos = Camera.main.ScreenToWorldPoint(new Vector3(MousePos.x + 10, MousePos.y - 10));
+        }
+
+        transform.localPosition = new Vector3(pos.x, pos.y, -2);
+        Debug.Log(transform.localPosition);
+
+    }
+
+    public void setPosition(Vector2 newPos)
+    {
+        MousePos = newPos;
+    }
+}
