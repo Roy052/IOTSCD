29fa67e66f1de8d125a15e8aa07551dd2592275c
crihit <rlagusgkr98@gmail.com>
2021-06-06 10:23:10 +0900

    Change mouse pointer not to go out of the screen

--- Assets/Scenes/Receiver.cs
+++ Assets/Scenes/Receiver.cs
@@ -97,5 +97,11 @@ public class Receiver : MonoBehaviour
 
             transform.position = transform.position + new Vector3(z, -x, 0) - new Vector3(minusX, minusY,0);
         }
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+            if (pos.x < 0f) pos.x = 0f;
+            if (pos.x > 1f) pos.x = 1f;
+            if (pos.y < 0f) pos.y = 0f;
+            if (pos.y > 1f) pos.y = 1f;
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
     }
 }
